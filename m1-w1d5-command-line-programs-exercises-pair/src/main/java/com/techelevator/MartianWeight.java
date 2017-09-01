2e3264ca22b37527fa7f7dbe169d6b53e7ef1699
Lauren Agne <AgneLauren1@gmail.com>
2017-09-01 15:28:37 -0400

    Martian Weight

--- m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MartianWeight.java
+++ m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,7 +21,67 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		/*System.out.println("How many weights are you converting?: ");
+		int e = input.nextInt();
+		input.nextLine();	
+		String [] earthWeights = new String[e];    */ // defined the earthWeights string array and it is "e" blocks long.
+		
+		System.out.print("Enter a series of Earth weights (space-separated): ");
+		String userInput = input.nextLine();      
+		String [] earthWeights = userInput.split(" ");		// 
+		
+		double marsW = 0;
+		
+		double [] earthWeightDoubleVersion = new double [earthWeights.length];		
+		
+		for (int i=0 ; i<earthWeights.length ; i++ ){
+			earthWeightDoubleVersion[i] = Double.parseDouble(earthWeights[i]);
+			marsW = earthWeightDoubleVersion[i] * 0.378;
+			System.out.println(earthWeights[i] + "lbs. on Earth, is " + marsW + " lbs. on Mars.");
 
+			}
+		
+		
+		}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	/*Scanner input = new Scanner(System.in);
+		
+		System.out.print("How many weights are you converting?: ");
+		double numberOfWeights = input.nextDouble();
+		input.nextLine(); 
+		
+		System.out.print("Please enter first weight: ");
+		double weight1= input.nextDouble();
+		input.nextLine(); 
+		
+		double marsW; 
+		
+		System.out.println(earthW + " lbs. on Earth, is " + doWConversion(l, lengthType) ;{
+		double [] array; 
+		
+		double i=0;
+		
+		for (i=0; i<array.length;i++);{
+		marsW = array [i] * 0.378;
+		}
+		
+		System.out.println( array[0] + "lbs. on Earth, is " + marsW + " lbs. on Mars.")
+		{
+				
 	}
 
-}
+}*/
